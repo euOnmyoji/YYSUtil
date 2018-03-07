f30aaaa4dba81967cc12f4d743c50caea5672b8e
yinyangshi <1418780411@qq.com>
2018-03-07 22:01:33 +0800

    first

---
+++ src/yutang/yys/util/FilesModifiedTime.java
@@ -0,0 +1,43 @@
+package yutang.yys.util;
+
+import com.sun.istack.internal.NotNull;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.attribute.FileTime;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Objects;
+
+public class FilesModifiedTime {
+    private FileTime time;
+    private List<FilesModifiedTime> list = new ArrayList<>();
+
+    @SuppressWarnings("WeakerAccess")
+    public FilesModifiedTime(@NotNull Path path) throws IOException {
+        time = Files.getLastModifiedTime(path);
+        if (Files.isDirectory(path)) {
+            Iterator<Path> i = Files.list(path).iterator();
+            while (i.hasNext()) {
+                Path p = i.next();
+                list.add(new FilesModifiedTime(p));
+            }
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        FilesModifiedTime that = (FilesModifiedTime) o;
+        return Objects.equals(time, that.time) &&
+                Objects.equals(list, that.list);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(time, list);
+    }
+}
