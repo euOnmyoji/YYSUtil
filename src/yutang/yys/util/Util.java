e06c12a509cb8f73a13948229baca4dfbde248a0
yinyangshi <1418780411@qq.com>
2018-03-11 15:11:16 +0800

    delete some zz method

--- src/yutang/yys/util/Util.java
+++ src/yutang/yys/util/Util.java
@@ -1,49 +1,12 @@
 package yutang.yys.util;
 
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.StandardCopyOption;
-import java.util.Iterator;
-
 public class Util {
+
     /**
      * @param s 秒数
      * @return 小时 分 秒
      */
     public static long[] parseSeconds(long s) {
         return new long[]{s / 3600, (s % 3600) / 60, s % 60};
-    }
-
-    /**
-     * @param path  复制到哪个文件夹下的文件夹路径
-     * @param files 被复制的文件或文件夹
-     */
-    public static void copy(Path path, File... files) throws IOException {
-        if (files != null) {
-            for (File f : files) {
-                if (!Files.exists(path)) {
-                    Files.createDirectory(path);
-                }
-                if (f.isDirectory()) {
-                    copy(path.resolve(f.getName()), f.listFiles());
-                } else {
-                    Files.copy(f.toPath(), path.resolve(f.getName()), StandardCopyOption.REPLACE_EXISTING);
-                }
-            }
-        }
-    }
-
-    public static void deleteAll(Path... paths) throws IOException {
-        for (Path path : paths) {
-            if (Files.isDirectory(path)) {
-                Iterator<Path> ps = Files.list(path).iterator();
-                while (ps.hasNext()) {
-                    Files.delete(ps.next());
-                }
-            }
-            Files.delete(path);
-        }
     }
 }
